75d194b3843e135f132aa53f45b565fe5e4a334d
Krunal Hingu <krunal.hingu@gmail.com>
2026-03-12 18:59:42 +0530

    Update next-sitemap.config.js

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: "https://abhai-surti-pm.vercel.app", // your deployed site
+  siteUrl: "https://krunalhingu.vercel.app", // your deployed site
   generateRobotsTxt: true, // generate robots.txt file
   sitemapSize: 7000, // optional, splits if huge
   changefreq: "weekly", // how often your pages update
