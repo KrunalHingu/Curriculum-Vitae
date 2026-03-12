95eca8043e765f457bd9c5a845b628809b79deb2
Krunal Hingu <krunal.hingu@gmail.com>
2026-03-12 17:40:03 +0530

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,18 +1,15 @@
+import { HiOutlineHome } from "react-icons/hi";
 export default function Navbar() {
   return (
     <header className="fixed top-0 left-0 w-full bg-white/80 backdrop-blur border-b border-white/10 z-50">
       <div className="max-w-6xl mx-auto flex items-center justify-between px-6 py-4">
 
-        {/* Logo / Name */}
-        <h1 className="text-xl font-bold text-primary">
-          Krunal Hingu
-        </h1>
 
         {/* Navigation */}
         <nav className="hidden md:flex space-x-6 text-sm font-medium">
 
           <a href="#home" className="hover:text-primary transition">
-            Home
+            <HiOutlineHome />
           </a>
 
           <a href="#about" className="hover:text-primary transition">
